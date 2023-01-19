84ddf5f1ab3eaba601109acc067ee9798c2dda67
Vladimir <vladimir.mirnyy@metinvest.digital>
2023-01-19 22:22:07 +0200

    Part 3.

--- FSharpKoans/AboutDotNetCollections.fs
+++ FSharpKoans/AboutDotNetCollections.fs
@@ -1,11 +1,12 @@
 ﻿namespace FSharpKoans
+
 open FSharpKoans.Core
 open System.Collections.Generic
 
 //---------------------------------------------------------------
 // About .NET Collections
 //
-// Since F# is built for seamless interop with other CLR 
+// Since F# is built for seamless interop with other CLR
 // languages, you can use all of the basic .NET collections types
 // you're already familiar with if you're a C# or VB programmer.
 //---------------------------------------------------------------
@@ -13,54 +14,54 @@ open System.Collections.Generic
 module ``about dot net collections`` =
 
     [<Koan>]
-    let CreatingDotNetLists() =
+    let CreatingDotNetLists () =
         let fruits = new List<string>()
 
         fruits.Add("apple")
         fruits.Add("pear")
- 
-        AssertEquality fruits.[0] __
-        AssertEquality fruits.[1] __
+
+        AssertEquality fruits.[0] "apple"
+        AssertEquality fruits.[1] "pear"
 
     [<Koan>]
-    let CreatingDotNetDictionaries() =
+    let CreatingDotNetDictionaries () =
         let addressBook = new Dictionary<string, string>()
 
         addressBook.["Chris"] <- "Ann Arbor"
         addressBook.["SkillsMatter"] <- "London"
 
-        AssertEquality addressBook.["Chris"] __
-        AssertEquality addressBook.["SkillsMatter"] __
+        AssertEquality addressBook.["Chris"] "Ann Arbor"
+        AssertEquality addressBook.["SkillsMatter"] "London"
 
     [<Koan>]
-    let YouUseCombinatorsWithDotNetTypes() =
+    let YouUseCombinatorsWithDotNetTypes () =
         let addressBook = new Dictionary<string, string>()
 
         addressBook.["Chris"] <- "Ann Arbor"
         addressBook.["SkillsMatter"] <- "London"
 
-        let verboseBook = 
+        let verboseBook =
             addressBook
             |> Seq.map (fun kvp -> sprintf "Name: %s - City: %s" kvp.Key kvp.Value)
             |> Seq.toArray
 
         //NOTE: The seq type in F# is an alias for .NET's IEnumerable interface
-        //      Like the List and Array module, the Seq module contains functions 
-        //      that you can combine to perform operations on types implementing 
+        //      Like the List and Array module, the Seq module contains functions
+        //      that you can combine to perform operations on types implementing
         //      seq/IEnumerable.
 
-        AssertEquality verboseBook.[0] __
-        AssertEquality verboseBook.[1] __
+        AssertEquality verboseBook.[0] "Name: Chris - City: Ann Arbor"
+        AssertEquality verboseBook.[1] "Name: SkillsMatter - City: London"
 
     [<Koan>]
-    let SkippingElements() =
-        let original = [0..5]
+    let SkippingElements () =
+        let original = [ 0 .. 5 ]
         let result = Seq.skip 2 original
-        
-        AssertEquality result __
+
+        AssertEquality result (seq { 2 .. 5 })
 
     [<Koan>]
-    let FindingTheMax() =
+    let FindingTheMax () =
         let values = new List<int>()
 
         values.Add(11)
@@ -70,15 +71,20 @@ module ``about dot net collections`` =
         values.Add(3)
 
         let result = Seq.max values
-        
-        AssertEquality result __
-    
+
+        AssertEquality result 20
+
     [<Koan>]
-    let FindingTheMaxUsingACondition() =
-        let getNameLength (name:string) =
-            name.Length
-        
-        let names = [| "Harry"; "Lloyd"; "Nicholas"; "Mary"; "Joe"; |]
-        let result = Seq.maxBy getNameLength names 
-        
-        AssertEquality result __
+    let FindingTheMaxUsingACondition () =
+        let getNameLength (name: string) = name.Length
+
+        let names =
+            [| "Harry"
+               "Lloyd"
+               "Nicholas"
+               "Mary"
+               "Joe" |]
+
+        let result = Seq.maxBy getNameLength names
+
+        AssertEquality result "Nicholas"
